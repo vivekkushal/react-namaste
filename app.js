c065fa54faa853e5e780d99d9e4205e6ce4f67a2
vivekkushal <vivekjss1@gmail.com>
2025-02-28 08:39:56 +0530

    refactor: Add JSX

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement(
-  'h1',
-  { id: 'heading' },
-  'Hello World from React!'
+const heading = (
+  <h1 id="heading" tabIndex="5">
+    Namaste React from JSX 🚀
+  </h1>
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
