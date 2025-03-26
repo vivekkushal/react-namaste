5af2318cf6fb113f55e82be55377c6f796721c27
vivekkushal <vivekjss1@gmail.com>
2025-03-26 08:17:33 +0530

    feat: Routing paths

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 
 import Header from './components/Header';
 import Body from './components/Body';
+import About from './components/About';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 // App component
 const AppLayout = () => {
@@ -14,6 +17,17 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/components/About.js
@@ -0,0 +1,6 @@
+// About component
+const About = () => {
+  return <div className="about">About</div>;
+};
+
+export default About;
