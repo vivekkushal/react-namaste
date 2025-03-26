0724a42a0db4bf839fbf92247b6ad47c544265a8
vivekkushal <vivekjss1@gmail.com>
2025-03-26 21:02:00 +0530

    feat: Link paths

--- src/components/NavItems.js
+++ src/components/NavItems.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 // NavItems component
 const NavItems = () => {
@@ -11,9 +12,15 @@ const NavItems = () => {
   return (
     <div className="nav-items">
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
         <li>Cart</li>
         <button className="auth-btn" onClick={loginHandler}>
           {isLogin ? 'Logout' : 'Login'}
